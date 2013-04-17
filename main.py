10dacd73ca85d3a0bb04442d3866f15701668d86
David Lougheed <david.lougheed@gmail.com>
2013-04-17 16:02:59 -0400

    Made css settings color lists instead of svg to allow for easy change, re-enabled color dictionary in main.py (dont know if I should do this...) 20 minutes

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ g_data =([{"key": "set1", "values": [[1.0, 100], [2.0, 200], [3.0, 400]]},
 		{"key": "set3", "values": [[1.0, 220], [2.0, 140], [3.0, 320]]},
 		{"key": "set4", "values": [[1.0, 330], [2.0, 130], [3.0, 270]]}])
 
-#colorDict ={'Rset1': ["#FFF200", "#7065AD", "#EE2971", "#51B848"],'Rset2':["#297fff", "#7137f8", "#00d400", "#ff7f2a", "#ff0000","#808080", "#000000", "#ffcc00", "#37c8ab", "#ff2ad4"],'CBset3':["#A7CEE2","#2078B4", "#B4D88B", "#34A048", "#F69999", "#E21F26","#FDBF6E", "#F57E20"],'d3set10':["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd","#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"],'d3set20' : ["#1f77b4", "#aec7e8","#ff7f0e", "#ffbb78","#2ca02c", "#98df8a","#d62728", "#ff9896","#9467bd", "#c5b0d5","#8c564b", "#c49c94","#e377c2", "#f7b6d2","#7f7f7f", "#c7c7c7","#bcbd22", "#dbdb8d","#17becf", "#9edae5"],'d3set20b':["#393b79", "#5254a3", "#6b6ecf", "#9c9ede","#637939", "#8ca252", "#b5cf6b", "#cedb9c","#8c6d31", "#bd9e39", "#e7ba52", "#e7cb94","#843c39", "#ad494a", "#d6616b", "#e7969c","#7b4173", "#a55194", "#ce6dbd", "#de9ed6"],'d3set20c':["#3182bd", "#6baed6", "#9ecae1", "#c6dbef","#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2","#31a354", "#74c476", "#a1d99b", "#c7e9c0","#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb","#636363", "#969696", "#bdbdbd", "#d9d9d9"],'Rset3':["#ff6600", "#2a7fff", "#6f7c91", "#37c871","#00ffff", "#ffff00", "#ff7f2a", "#ff2a2a","#5f5fd3", "#8a6f91", "#37c871", "#8d5fd3","#87aade", "#d3bc5f", "#918a6f", "#d35f5f","#8dd35f", "#ff80b2", "#93aca7", "#37c871"]}
+colorDict ={'Rset1': ["#FFF200", "#7065AD", "#EE2971", "#51B848"],'Rset2':["#297fff", "#7137f8", "#00d400", "#ff7f2a", "#ff0000","#808080", "#000000", "#ffcc00", "#37c8ab", "#ff2ad4"],'CBset3':["#A7CEE2","#2078B4", "#B4D88B", "#34A048", "#F69999", "#E21F26","#FDBF6E", "#F57E20"],'d3set10':["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd","#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"],'d3set20' : ["#1f77b4", "#aec7e8","#ff7f0e", "#ffbb78","#2ca02c", "#98df8a","#d62728", "#ff9896","#9467bd", "#c5b0d5","#8c564b", "#c49c94","#e377c2", "#f7b6d2","#7f7f7f", "#c7c7c7","#bcbd22", "#dbdb8d","#17becf", "#9edae5"],'d3set20b':["#393b79", "#5254a3", "#6b6ecf", "#9c9ede","#637939", "#8ca252", "#b5cf6b", "#cedb9c","#8c6d31", "#bd9e39", "#e7ba52", "#e7cb94","#843c39", "#ad494a", "#d6616b", "#e7969c","#7b4173", "#a55194", "#ce6dbd", "#de9ed6"],'d3set20c':["#3182bd", "#6baed6", "#9ecae1", "#c6dbef","#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2","#31a354", "#74c476", "#a1d99b", "#c7e9c0","#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb","#636363", "#969696", "#bdbdbd", "#d9d9d9"],'Rset3':["#ff6600", "#2a7fff", "#6f7c91", "#37c871","#00ffff", "#ffff00", "#ff7f2a", "#ff2a2a","#5f5fd3", "#8a6f91", "#37c871", "#8d5fd3","#87aade", "#d3bc5f", "#918a6f", "#d35f5f","#8dd35f", "#ff80b2", "#93aca7", "#37c871"]}
+# !!!! I RE-ENABLED THE COLOR DICT, IF THERE IS SOME ALTERNATVE THAT I'M MISSING PLEASE INFORM
+
 # cSet = 'Rset2'
 # axisdict = {'0d':'d3.format(\',r\')','1d':'d3.format(\',.1f\')','2d':'d3.format(\',.2f\')','3d':'d3.format(\',.3f\')','strf':'function(d) { return d3.time.format(\'%x\')(new Date(d))}'}
 
@@ -514,7 +516,7 @@ class DeleteSet(Plot3Handler):
 class MainSettingHandler(Plot3Handler):
 	def get(self):
 		[loggedIn,admin,UN] = self.checkCookies()
-		if loggedIn: self.render('settings_main.html')
+		if loggedIn: self.render('settings_main.html',colorDict=colorDict)
 		else: self.redirect('/logout')
 		
 
@@ -540,7 +542,7 @@ class MainSettingHandler(Plot3Handler):
 class PlotSettingHandler(Plot3Handler):
 	def get(self):
 		[loggedIn,admin,UN] = self.checkCookies()
-		if loggedIn: self.render('settings.html',admin=loggedIn,UN=UN)
+		if loggedIn: self.render('settings.html',admin=loggedIn,UN=UN,colorDict=colorDict)
 		else: self.response.out.write('You are not an logged in')
 
 	def post(self):
@@ -922,7 +924,7 @@ class RandomPlotSet(Plot3Handler):
 
 class RandomPlotSettingHandler(Plot3Handler):
 	def get(self):
-		self.render('settings.html',admin=True,randset=True) #allow any property to be changed
+		self.render('settings.html',admin=True,randset=True,colorDict=colorDict) #allow any property to be changed
 		
 		
 	def post(self):
